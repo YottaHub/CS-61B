a94bab7c383a0be95dbef7a77701dfb84bc4b7bd
yotta <yotta.yottas@gmail.com>
2022-10-21 14:53:56 +0800

    implement bfs

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,46 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private int t;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> fringe = new LinkedList<>();
+        fringe.add(s);
+        while (!fringe.isEmpty()) {
+            int c = fringe.poll();
+            marked[c] = true;
+            announce();
+            if (c == t) {
+                return;
+            }
+            for (int w : maze.adj(c)) {
+                if (!marked[w]) {
+                    distTo[w] = distTo[c] + 1;
+                    edgeTo[w] = c;
+                    fringe.add(w);
+                    announce();
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,44 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int[] parent;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        parent = new int[maze.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        int s = maze.xyTo1D(1, 1);
+        parent[s] = s;
+        Stack<Integer> fringe = new Stack<>();
+        fringe.push(s);
+        while (!fringe.isEmpty()) {
+            int v = fringe.pop();
+            marked[v] = true;
+            announce();
+            for (int w : maze.adj(v)) {
+                if (marked[w]) {
+                    if (w != parent[v]) {
+                        int c = v;
+                        while (c != w) {
+                            edgeTo[c] = parent[c];
+                            c = parent[c];
+                            announce();
+                        }
+                        return;
+                    }
+                } else {
+                    fringe.push(w);
+                    distTo[w] = distTo[v] + 1;
+                    parent[w] = v;
+                    announce();
+                }
+            }
+        }
     }
-
-    // Helper methods go here
 }
 
